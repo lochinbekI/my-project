0bc20087606819bc54d3357a3c18bcf9f41eda29
lochinbek <lochin.israilov@mail.ru>
2020-04-06 18:30:32 +0500

    foote was done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,30 +4,39 @@
             <div class="container">
                 <div class="top_footer">
                     <div class="tofoter">
-                        <form action="">
-                            <div class="tofoter_form">
-                                <h6 class="tofoter_form_tth6">Остались вопросы?</h6>
-                                <h1 class="tofoter_form_tth1">Связаться с нами</h1>
-                                <input type="text" placeholder="Имя">
-                                <input type="text" placeholder="Телефон">
-                                <div class="top_footer_select">
-                                    <select name="" id="">
-                                        <option selected disabled value="">Какая услуга вас интересует</option>
-                                        <option value="">usliga 1</option>
-                                        <option value="">usliga 2</option>
-                                        <option value="">usliga 3</option>
-                                        <option value="">usliga 4</option>
-                                    </select>
-                                    <span>
+                        <ValidationObserver v-slot="{ handleSubmit }">
+                            <form @submit.prevent="handleSubmit(onSubmit)">
+                                <div class="tofoter_form">
+                                    <h6 class="tofoter_form_tth6">Остались вопросы?</h6>
+                                    <h1 class="tofoter_form_tth1">Связаться с нами</h1>
+
+                                    <ValidationProvider name="Имя" rules="required|alpha_dash" v-slot="{ errors }">
+                                        <input class="mb_0px" v-model="value2" type="text" placeholder="Имя">
+                                        <span class="veevalid_mas">{{ errors[0] }}</span>
+                                    </ValidationProvider>
+                                    <ValidationProvider rules="required" v-slot="{ errors }">
+                                        <input class="mb_0px" v-model="value" placeholder="Телефон" type="text" v-mask="'+999(99)-99-99-99'" />
+                                        <span class="veevalid_mas">{{ errors[0] }}</span>
+                                    </ValidationProvider>
+                                    <div class="top_footer_select">
+                                        <select name="" id="">
+                                            <option selected disabled value="">Какая услуга вас интересует</option>
+                                            <option value="">usliga 1</option>
+                                            <option value="">usliga 2</option>
+                                            <option value="">usliga 3</option>
+                                            <option value="">usliga 4</option>
+                                        </select>
+                                        <span>
                                         <i class="fa fa-angle-down" aria-hidden="true"></i>
                                     </span>
+                                    </div>
+                                    <input type="text" placeholder="Сообщение">
+                                    <button class="header_recall header_recall_foter">
+                                        Отправить
+                                    </button>
                                 </div>
-                                <input type="text" placeholder="Сообщение">
-                                <button class="header_recall header_recall_foter">
-                                    Отправить
-                                </button>
-                            </div>
-                        </form>
+                            </form>
+                        </ValidationObserver>
                     </div>
                     <div class="tofoter_right">
                         <h1 class="tofoter_right_h1">Контакты</h1>
@@ -83,17 +92,65 @@
                     </div>
                 </div>
             </div>
+
             <div class="gradient_all gradient_all_height_6px"></div>
         </footer>
+
+
+
+
+
     </div>
 </template>
 
 <script>
+    import { extend } from 'vee-validate';
+    import { required, alpha, alpha_dash } from 'vee-validate/dist/rules';
+
+    // No message specified.
+    extend('alpha', alpha);
+    extend('alpha_dash', alpha_dash);
+
+    // Override the default message.
+    extend('required', {
+        ...required,
+        message: 'This field is required'
+    });
+
     export default {
-        name: 'Footer'
+        name: 'Footer',
+        data: () => ({
+            value: '',
+            value2: '',
+
+
+            firstName: '',
+            lastName: '',
+            email: ''
+        }),
+        methods: {
+            onSubmit()
+            {
+                alert('Form has been submitted!');
+            },
+            sendAccounts()
+            {
+
+            }
+        }
+
     };
 </script>
 
 <style scoped>
+    .veevalid_mas {
+        margin-bottom: 15px;
+        color: #E33985;
+        display: block;
+        padding-left: 15px;
+    }
 
+    .mb_0px {
+        margin-bottom: 0 !important;
+    }
 </style>
